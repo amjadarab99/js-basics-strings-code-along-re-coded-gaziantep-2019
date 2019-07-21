d42e472fa7b2634809480961f7f7fa154e3e4a95
Amjad Arab <amjad.arab99@gmail.com>
2019-07-21 10:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
 
-let shortGreeting = "Welcome, " + firstInitial;
+let shortGreeting =  firstInitial;
 const firstInitial = currentUser[0];
-const restOfName = currentUser.slice(1);
+
 
